e04d26041ade2641c8becd63d920a0a01eb5c34a
Edvard Pedersen <edvard.pedersen@uit.no>
2020-02-06 11:38:43 +0100

    update to python 3, clean up formatting

--- main.py
+++ main.py
@@ -2,73 +2,93 @@ import logging, logging.config
 import time
 import argparse
 import sys
-import ConfigParser
 import cProfile
 import re
 
 class Main():
-	def __init__(self):
-		self._parse_args()
-		self._setup_logging()
-		self._setup_timing()
+    def __init__(self):
+        self.config = self._parse_args()
+        self.logger = self._setup_logging()
+        self.timer = self._setup_timing()
 
-	def _parse_args(self):
-		# Parses command line arguments, new ones are added here
-		parser = argparse.ArgumentParser(description='Small script to do X')
-		parser.add_argument('-l', '--log_config', default="log_config", help="Use custom logging configuration file")
-		parser.add_argument('-p', '--profile', dest='profile', default=False, const=True, action='store_const', help="Profile the user code")
-		parser.add_argument('-t', '--test', dest='test', default=False, const=True, action='store_const', help="Run unit tests instead of running the program")
-		self.config = parser.parse_args()
+    def _parse_args(self):
+        # Parses command line arguments, new ones are added here
+        parser = argparse.ArgumentParser(description='Small script to do X')
+        parser.add_argument('-l',
+                            '--log_config',
+                            default="log_config",
+                            help="Use custom logging configuration file")
+        parser.add_argument('-p',
+                            '--profile',
+                            dest='profile',
+                            default=False,
+                            const=True,
+                            action='store_const',
+                            help="Profile the user code")
+        parser.add_argument('-t',
+                            '--test',
+                            dest='test',
+                            default=False,
+                            const=True,
+                            action='store_const',
+                            help="Run unit tests instead of running the program")
+        return parser.parse_args()
 
-	def _setup_logging(self):
-		# Attempts to load a logging config from disk, if it is not found a standard one is created
-		try:
-			logging.config.fileConfig(self.config.log_config)
-		except Exception as E:
-			tempLogger = logging.getLogger(__name__)
-			tempLogger.setLevel(logging.DEBUG)
-			handler = logging.StreamHandler()
-			handler.setLevel(logging.DEBUG)
-			handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-			tempLogger.addHandler(handler)
-			tempLogger.warn("Unable to load logging configuration, using defaults. Error was: " + str(E))
+    def _setup_logging(self):
+        # Attempts to load a logging config from disk, if it is not found a standard one is created
+        try:
+            logging.config.fileConfig(self.config.log_config)
+        except Exception as E:
+            tempLogger = logging.getLogger(__name__)
+            tempLogger.setLevel(logging.DEBUG)
+            handler = logging.StreamHandler()
+            handler.setLevel(logging.DEBUG)
+            standard_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            handler.setFormatter(logging.Formatter(standard_format))
+            tempLogger.addHandler(handler)
+            error = "Unable to load logging configuration, using defaults. Error was: " + str(E)
+            tempLogger.warn(error)
 
-		self.logger = logging.getLogger(__name__)
-		self.logger.info("Logger set up")
+        logger = logging.getLogger(__name__)
+        logger.info("Logger set up")
+        return logger
 
-	def _setup_timing(self):
-		self.timer = time.time()
+    def _setup_timing(self):
+        return time.time()
 
-	def start_timer(self):
-		''' Reset the timer '''
-		self.timer = time.time()
+    def start_timer(self):
+        ''' Reset the timer '''
+        self.timer = time.time()
 
-	def stop_timer(self):
-		''' Reset the timer, and return the time between the current call and previous call, or the previous call to start_timer() '''
-		difference = time.time() - self.timer
-		self.timer = time.time()
-		return difference
+    def stop_timer(self):
+        '''
+        Reset the timer, and return the time between the current call and previous call,
+        or the previous call to start_timer()
+        '''
+        difference = time.time() - self.timer
+        self.timer = time.time()
+        return difference
 
-	def run(self):
-		# Calls the user_main method to run the script
-		if self.config.test:
-			try:
-				import our_tests
-				our_tests.run()
-			except ImportError as E:
-				self.logger.critical("Unable to run tests " + str(E))
-		elif self.config.profile:
-			cProfile.runctx('self.user_main()', globals(), locals(), sort='tottime')
-		else:
-			self.user_main()
+    def run(self):
+        # Calls the user_main method to run the script
+        if self.config.test:
+            try:
+                import our_tests
+                our_tests.run()
+            except ImportError as E:
+                self.logger.critical("Unable to run tests " + str(E))
+        elif self.config.profile:
+            cProfile.runctx('self.user_main()', globals(), locals(), sort='tottime')
+        else:
+            self.user_main()
 
-	def user_main(self):
-		#User code goes here
-		x = 0
-		for i in range(100):
-			x = x + i
-			print x
+    def user_main(self):
+        #User code goes here
+        x = 0
+        for i in range(100):
+            x = x + i
+            print(x)
 
 if __name__ == "__main__":
-	app = Main()
-	app.run()
+        app = Main()
+        app.run()
--- our_tests.py
+++ our_tests.py
@@ -6,31 +6,29 @@ import logging
 import time
 
 def run():
-	unittest.TextTestRunner(verbosity=2).run(suite())
+    unittest.TextTestRunner(verbosity=2).run(suite())
 
 def suite():
-	return unittest.TestLoader().loadTestsFromTestCase(TestSetup)
-	
+    return unittest.TestLoader().loadTestsFromTestCase(TestSetup)
+
 class TestSetup(unittest.TestCase):
-	def setUp(self):
-		self.main = main.Main()
+    def setUp(self):
+        self.main = main.Main()
 
-	def tearDown(self):
-		self.main = None
+    def tearDown(self):
+        self.main = None
 
-	def test_config(self):
-		self.assertIsInstance(self.main.timer, float)
-		self.assertIsInstance(self.main.config, argparse.Namespace)
-		self.assertIsInstance(self.main.logger, logging.Logger)
+    def test_config(self):
+        self.assertIsInstance(self.main.timer, float)
+        self.assertIsInstance(self.main.config, argparse.Namespace)
+        self.assertIsInstance(self.main.logger, logging.Logger)
 
-	def test_timer(self):
-		self.main.start_timer()
-		time.sleep(0.1)
-		time_taken = self.main.stop_timer()
-		self.assertTrue(time_taken > 0.05)
-		self.assertTrue(self.main.stop_timer() < 0.05)
-		
-		
-		
+    def test_timer(self):
+        self.main.start_timer()
+        time.sleep(0.1)
+        time_taken = self.main.stop_timer()
+        self.assertTrue(time_taken > 0.05)
+        self.assertTrue(self.main.stop_timer() < 0.05)
+
 if __name__ == "__main__":
-	run()
+    run()
